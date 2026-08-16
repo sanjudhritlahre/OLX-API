5112566bb8ad4e54fc91b42bd313a10bef706673
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-08-17 02:11:30 +0530

    Setting Up PostgreSql

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,15 +7,22 @@ import (
 	"time"
 
 	"github.com/sanjudhritlahre/olx-api/internal/config"
+	"github.com/sanjudhritlahre/olx-api/internal/db"
 	"github.com/sanjudhritlahre/olx-api/internal/handlers"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	
+	_, err := db.Connect(cfg.DatabaseUrl)
+	if err != nil {
+		log.Fatalf("main.db.connect: %v", err)
+	}
 
+	fmt.Println("Database Connected.")
 	fmt.Println("OLX-API server is running...")
+	
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("GET /healthz", handlers.Healthz)
 
 	srv := http.Server{
