e7d9322a50f782b36776054d42e0ba8bcedf543e
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-08-23 19:41:55 +0530

    Fetching Database Records & Returning JSON

--- internal/handlers/listings.go
+++ internal/handlers/listings.go
@@ -2,11 +2,57 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 )
+
+type listing struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Price       string    `json:"price"`
+	City        string    `json:"city"`
+	CreatedAt   time.Time `json:"created_at"`
+}
 
 func Listings(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("All Okay!"))
+		rows, err := db.Query(
+			`SELECT id, title, description, price, city, created_at
+			FROM listings
+			ORDER BY created_at DESC
+			LIMIT 100`)
+		if err != nil {
+			log.Printf("query: %v", err)
+			http.Error(w, "internal error", http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		listings := []listing{}
+		for rows.Next() {
+			var l listing
+
+			if err := rows.Scan(&l.ID, &l.Title, &l.Description, &l.Price, &l.City, &l.CreatedAt); err != nil {
+				log.Printf("rows.scan: %v", err)
+				http.Error(w, "internal error", http.StatusInternalServerError)
+				return
+			}
+
+			listings = append(listings, l)
+		}
+
+		if err := rows.Err(); err != nil {
+			log.Printf("rows.Err: %v", err)
+			http.Error(w, "internal error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		_ = json.NewEncoder(w).Encode(listings)
 	}
 }
